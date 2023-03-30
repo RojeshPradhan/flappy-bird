f138590f1bd7df736f61893f134cb9c263be9b02
RojeshPradhan <crimsonrojesh03@gmail.com>
2023-03-30 11:28:12 +0545

    leftover

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flap/barrier.dart';
 import 'package:flutter/material.dart';
 
 import 'bird.dart';
@@ -12,20 +13,125 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdY = 0;
+  static double birdY = 0;
+  double initialPos = birdY;
+  double height = 0;
+  double time = 0;
+  double gravity = -4.9; //how strong gravity is
+  double velocity = 3.5; //how strong the jump is
+  double birdWidth = 0.1;
+  double birdHeight = 0.1;
+  //game settings
+  bool gameHasStarted = false;
+
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.5;
+  List<List<double>> barrierHeight = [
+    [0.6, 0.4],
+    [0.4, 0.6],
+  ];
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      height = gravity * time * time + 30 * time;
+
+      setState(() {
+        birdY = initialPos - height;
+      });
+
+      if (birdIsDead()) {
+        timer.cancel();
+        _showDialog();
+      }
+      moveMap();
+
+      time += 0.1;
+    });
+  }
+
+  void moveMap() {
+    for (int i = 0; i < barrierX.length; i++) {
+      setState(() {
+        barrierX[i] -= 0.005;
+      });
+
+      if (barrierX[i] < -1.5) {
+        barrierX[i] += 3;
+      }
+    }
+  }
+
+  void resetGame() {
+    Navigator.pop(context);
+    setState(() {
+      birdY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.brown,
+          title: const Center(
+            child: Text(
+              "G A M E O V E R",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          actions: [
+            GestureDetector(
+              onTap: resetGame,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  color: Colors.white,
+                  child: const Text(
+                    'PLAY AGAIN',
+                    style: TextStyle(color: Colors.brown),
+                  ),
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
 
   void jump() {
-    Timer.periodic(Duration(milliseconds: 50), (timer) {
-      setState(() {
-        birdY -= 0.05;
-      });
+    setState(() {
+      time = 0;
+      initialPos = birdY;
     });
+  }
+
+  bool birdIsDead() {
+    if (birdY < -1 || birdY > 1) {
+      return true;
+    }
+    for (int i = 0; i < barrierX.length; i++) {
+      if (barrierX[i] <= birdWidth &&
+          barrierX[i] + barrierWidth >= -birdWidth &&
+          (birdY <= -1 + barrierHeight[i][0] ||
+              birdY + birdHeight >= 1 - barrierHeight[i][1])) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: jump,
+        onTap: gameHasStarted ? jump : startGame(),
         child: Scaffold(
           body: Column(
             children: [
@@ -38,7 +144,41 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         MyBird(
                           birdY: birdY,
-                        )
+                          birdWidth: birdWidth,
+                          birdHeight: birdHeight,
+                        ),
+                        MyCoverScreen(gameHasStarted: gameHasStarted),
+                        MyBarrier(
+                          barrierX: barrierX[0],
+                          barrierWidth: barrierWidth,
+                          barrierHeight: barrierHeight[0][0],
+                          isThisBottomBarrier: false,
+                        ),
+                        MyBarrier(
+                          barrierX: barrierX[0],
+                          barrierWidth: barrierWidth,
+                          barrierHeight: barrierHeight[0][1],
+                          isThisBottomBarrier: true,
+                        ),
+                        MyBarrier(
+                          barrierX: barrierX[1],
+                          barrierWidth: barrierWidth,
+                          barrierHeight: barrierHeight[1][0],
+                          isThisBottomBarrier: false,
+                        ),
+                        MyBarrier(
+                          barrierX: barrierX[1],
+                          barrierWidth: barrierWidth,
+                          barrierHeight: barrierHeight[1][1],
+                          isThisBottomBarrier: true,
+                        ),
+                        Container(
+                            alignment: const Alignment(0, -0.5),
+                            child: Text(
+                              gameHasStarted ? '' : 'T A P  T O  P L A Y',
+                              style: const TextStyle(
+                                  color: Colors.purple, fontSize: 20),
+                            ))
                       ],
                     ),
                   ),
@@ -54,3 +194,5 @@ class _HomePageState extends State<HomePage> {
         ));
   }
 }
+
+
--- lib/bird.dart
+++ lib/bird.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 class MyBird extends StatelessWidget {
   final birdY;
+  final double birdWidth;
+  final double birdHeight;
 
-  MyBird({required this.birdY});
+  MyBird(
+      {required this.birdY, required this.birdWidth, required this.birdHeight});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment(0, birdY),
-        height: 50,
-        width: 50,
-        child: Image.asset('lib/images/bird.png'));
+        alignment: Alignment(0, (2 * birdY + birdHeight) / (2 - birdHeight)),
+        child: Image.asset(
+          'lib/images/bird.png',
+          width: MediaQuery.of(context).size.width * birdWidth / 2,
+          height: MediaQuery.of(context).size.height * 3 / 4 * birdHeight / 2,
+          fit: BoxFit.fill,
+        ));
   }
 }
